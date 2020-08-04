82c428576adb2220289a48d3865765ff9b03d5d4
Levan Gogoladze <l_gogoladze1@cu.edu.ge>
2020-08-04 22:42:59 +0400

    workin on setup project

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/app.js",
-  devtool: "source-maps",
   watch: true,
-  watchoptions: {
+  watchOptions: {
     ignored: /node_modules/,
   },
   plugins: [
-    new CleanWebpackPlugin(["dist"]),
+    // new CleanWebpackPlugin(["dist"]),
+    new HtmlWebPackPlugin({
+      template: "./authorization.html",
+    }),
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
@@ -31,6 +34,24 @@ module.exports = {
             presets: ["@babel/preset-env"],
           },
         },
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            outputPath: "images",
+            name: "[name].[ext]",
+          },
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
       },
     ],
   },
