bd90ab587992aab6d2c25ec41e7c353944f16c1f
Levan Gogoladze <l_gogoladze1@cu.edu.ge>
2020-08-07 18:35:22 +0400

    working on transactions page

--- web/js/app.js
+++ web/js/app.js
@@ -1,32 +1,55 @@
 import jQuery from "jquery";
 window.$ = window.jQuery = jQuery;
+import "../../node_modules/bootstrap/js/dist/dropdown";
 
-import "bootstrap";
 
 $(".hamburger-btn").click(function () {
   if ($(this).hasClass("clicked")) {
     $(this).removeClass("clicked");
     $(".sidebar").css("width", "80px");
     $(".main").css("padding-left", "80px");
-    hideElements();
+    $(".bottom").removeClass("collapsedBottom");
+    shrinkNavItem();
   } else {
     $(this).addClass("clicked");
     $(".sidebar").css("width", "265px");
     $(".main").css("padding-left", "265px");
-    showElements();
+    $(".bottom").addClass("collapsedBottom");
+    expnadNavItem();
   }
+  showHideElements();
 });
 
-function hideElements() {
-  $(".show").each(function () {
-    $(this).removeClass("show");
-    $(this).addClass("hide");
+function showHideElements() {
+  let hiddenElements = $(".hide");
+  let shownElements = $(".show");
+  hiddenElements.each(function () {
+    $(this).toggleClass(["show", "hide"]);
+  });
+  shownElements.each(function () {
+    $(this).toggleClass(["show", "hide"]);
   });
 }
 
-function showElements() {
-  $(".hide").each(function () {
-    $(this).removeClass("hide");
-    $(this).addClass("show");
+function expnadNavItem() {
+  $(".nav-item").each(function () {
+    $(this).addClass("expanded-nav-item");
   });
 }
+
+function shrinkNavItem() {
+  $(".nav-item").each(function () {
+    $(this).removeClass("expanded-nav-item");
+  });
+}
+
+
+$(".dropdwon-nav-item").click(function () {
+    $('.dp').toggleClass("show-dropdown");
+    $('i').toggleClass("rotated");
+});
+
+
+$('.datepicker').datepicker({
+  inline: true
+});
